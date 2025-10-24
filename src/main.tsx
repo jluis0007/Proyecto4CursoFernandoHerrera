1cbf0b5f397d551fdd37f22b32da4defd1d2427f
jluis0007 <jluis.0007@hoatmail.com>
2025-10-24 14:52:59 -0600

    158. Use API + Suspense en acción

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,47 @@
-import { StrictMode } from "react";
+import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 
-//import { HooksApp } from './HooksApp';
-/* import { TrafficLight } from "./01-useState/TrafficLight";
-import { TrafficLightWithEffect } from "./02-useEffect/TrafficLightWithEffect"; */
-// import { TrafficLightWithHook } from "./02-useEffect/TrafficLightWithHook";
-/* import { PokemonPage } from "./02-useEffect/03-examples/PokemonPage"; */
-/* import { FocusScreen } from "./04-useRef/FocusScreen"; */
-/* import { TasksApp } from "./05-useReducer/TaskApp"; */
-/* import { ScrambleWords } from "./05-useReducer/ScrambleWords"; */
-/* import { MemoHook } from "./06-memos/MemoHook"; */
+import { Toaster } from "sonner";
+
+// import { HooksApp } from './HooksApp';
+// import { TrafficLight } from './01-useState/TrafficLight';
+// import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect';
+// import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook';
+// import { PokemonPage } from './03-examples/PokemonPage';
+// import { FocusScreen } from './04-useRef/FocusScreen';
+// import { TasksApp } from './05-useReducer/TaskApp';
+// import { ScrambleWords } from './05-useReducer/ScrambleWords';
+// import { MemoHook } from './06-memos/MemoHook';
+// import { MemoCounter } from './06-memos/MemoCounter';
+// import { InstagromApp } from './07-useOptimistic/InstagromApp';
+import { ClientInformation } from "./08-use-suspense/ClientInformation";
 
 import "./index.css";
-import { MemoCounter } from "./06-memos/ui/MemoCounter";
+import { getUserAction } from "./08-use-suspense/api/get-user.action";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Toaster />
+    {/* <HooksApp /> */}
     {/* <TrafficLight /> */}
     {/* <TrafficLightWithEffect /> */}
     {/* <TrafficLightWithHook /> */}
     {/* <PokemonPage /> */}
-    {/* <FocusScreen/> */}
-    {/* <TasksApp/> */}
+    {/* <FocusScreen /> */}
+    {/* <TasksApp /> */}
     {/* <ScrambleWords /> */}
     {/* <MemoHook /> */}
-    <MemoCounter />
+    {/* <MemoCounter /> */}
+    {/* <InstagromApp /> */}
+
+    <Suspense
+      fallback={
+        <div className="bg-gradient flex flex-col">
+          <h1 className="text-2xl">Cargando</h1>
+        </div>
+      }
+    >
+      <ClientInformation getUser={getUserAction(1001)} />
+    </Suspense>
   </StrictMode>
 );
